c3feeedc3108506cc7735637bd734a6c617a807c
pjhpoi66 <60897503+pjhpoi66@users.noreply.github.com>
2025-08-29 05:32:31 +0900

    Initialize project structure with Spring Boot, Gradle, and core application components.

---
+++ src/main/java/com/joongho/geminichat/GeminiChatApplication.java
@@ -0,0 +1,22 @@
+package com.joongho.geminichat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@Slf4j
+public class GeminiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiChatApplication.class, args);
+
+        log.info("=========================================================");
+        log.info("Build Instance : {}", SpringApplication.class.getName());
+        log.info("Build Time : {}", LocalDateTime.now());
+        log.info("=========================================================");
+    }
+
+}
